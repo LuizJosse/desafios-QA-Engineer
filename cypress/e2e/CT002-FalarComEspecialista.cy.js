7e2207c664349f75b20864ed3aa582fbf8fdb838
LuizJosse <luiznetto12@hotmail.com>
2025-09-12 20:12:02 -0300

    Atulizações CT02

--- cypress/e2e/CT002-FalarComEspecialista.cy.js
+++ cypress/e2e/CT002-FalarComEspecialista.cy.js
@@ -14,7 +14,7 @@ describe('Formulário Falar com Especialista', () => {
   it('Preenche o formulário com dados fake', () => {
     // Gera os dados antes
     const nomeFake = faker.person.firstName()
-     const sobrenomeFake = faker.person.lastName()
+    const sobrenomeFake = faker.person.lastName()
     const emailFake = faker.internet.email()
     const telefoneFake = faker.phone.number('###########')
     const mensagemFake = faker.lorem.sentence()
@@ -33,15 +33,53 @@ describe('Formulário Falar com Especialista', () => {
         .first()
         .type(sobrenomeFake)
       
-     cy.get('input[name="mobile"]', { timeout: 10000 })
+    cy.get('input[name="mobile"]', { timeout: 10000 })
         .should('be.visible')
         .first()
         .type(telefoneFake)
+    
+    cy.get('input[name="email"]', { timeout: 10000 })
+        .should('be.visible')
+        .first()
+        .type("luiz.neto@stigmasystem.com.br")
 
     cy.get('input[name="company"]', { timeout: 10000 })
         .should('be.visible')
         .first()
-      1  .type(mensagemFake)
+        .type(mensagemFake)
+    
+    cy.get('select#cargo') 
+       .should('be.visible')
+       .select('Autônomo(a) ou Consultor(a)') 
+
+    cy.get('select#funcionarios') 
+       .should('be.visible')
+       .select('51 - 200') 
+    
+    cy.get('select#segmento') 
+       .should('be.visible')
+       .select('Serviços') 
+
+    cy.get('select#canal') 
+       .should('be.visible')
+       .select('WhatsApp') 
+
+    cy.get('select#verificacao') 
+      .should('be.visible')
+      .select('até 1.000') 
+
+    cy.contains('button', 'Falar com especialista')
+      .should('be.visible')
+      .click()
+
+    cy.contains('h4', 'Interesse enviado com sucesso!')
+     .should('be.visible')
+    
+    cy.get('.styles_thanksContainer__ko7k1 > .button').click()
+    cy.get('h1').should('be.visible').and('contain.text', 'Reduza fraudes e gerencie riscos')
+
+
+
     })
   })
 })
